d3a3a63784e8431d56b7ca60328ffae485a638a5
Carl Waldron <Jarvis@Jarvis-V4.home>
2017-03-14 15:20:26 -0400

    second init

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,29 @@
-// See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
-};
+module.exports = {
+  // paths: {
+  //   public: 'static',
+  //   watched: [
+  //     'app',
+  //     'assets',
+  //     'styles',
+  //     'vendor',
+  //   ]
+  // },
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/full.js': /^app/,
+        'js/angular.min.js': /^app/,
+        'js/vendor.js': /^(vendor|bower_components)/,
+        'test/js/test.js': /^test(\/|\\)(?!vendor)/,
+        'test/js/test-vendor.js': /^test(\/|\\)(?=vendor)/
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'css/full.css': /^app/,
+        'css/vendor.css': /^(vendor|bower_components)/
+      }
+    },
+    templates: {joinTo: 'full.js'}
+  }
+}
